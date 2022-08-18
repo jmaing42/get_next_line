7cf2931d3ceb738290e5613335a9a7b411be8296
맹주영 <mjy9088@naver.com>
2022-08-19 02:00:05 +0900

    remove outdated test

--- src/before_test.sh
+++
@@ -1,3 +0,0 @@
-cp get_next_line_bonus.h get_next_line.h
-cp get_next_line_bonus.c get_next_line.c
-cp get_next_line_utils_bonus.c get_next_line_utils.c
--- src/test.sh
+++
@@ -1,6 +0,0 @@
-clang \
-	-Wall -Wextra -Werror \
-	-fsanitize=address -g3 \
-	test.c get_next_line_utils_bonus.c \
-	-o test.exe \
-	&& ./test.exe
